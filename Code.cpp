6b6b272163349858bd76dcabb648cc73882c605d
bani bani <itzbani2293@gmail.com>
2023-12-06 15:45:17 +0700

    Lihat, Menu

--- Code.cpp
+++ Code.cpp
@@ -6,6 +6,16 @@ struct Data {
     string nama,pekerjaan,status,alamat,tipe,ket,nik,kk,penghasilan;
 };
 
+// Fungsi untuk menampilkan seluruh data
+void tampilkanData(const vector<Data>& data) {
+    cout << endl << "Data saat ini:" << endl;
+    for (const auto& d : data) {
+        cout << "Nama: " << d.nama << ", No KK: " << d.kk << ", No NIK: " << d.nik << ", Pekerjaan: " << d.pekerjaan;
+        cout << ", Penghasilan: Rp." << d.penghasilan << ", Status: " << d.status << ", Alamat Desa: " << d.alamat << ", Tipe Sembako: " << d.tipe;
+		cout << ", Ket: " << d.ket << endl << endl;
+    }
+}
+
 int main() {
     vector<Data> data;
 
@@ -13,9 +23,8 @@ int main() {
     while (true) {
         cout << "Menu:" << endl;
         cout << "1. Tampilkan Data" << endl;
-        cout << "2. Edit Data" << endl;
-        cout << "3. Keluar" << endl;
-        cout << "Pilih menu (1-3): ";
+        cout << "2. Keluar" << endl;
+        cout << "Pilih menu (1-2): ";
         cin >> pilihan;
 
         switch (pilihan) {
@@ -23,9 +32,6 @@ int main() {
                 tampilkanData(data);
                 break;
             case 2:
-                editData(data);
-                break;
-            case 3:
                 return 0;
             default:
                 cout << "Pilihan tidak valid." << endl;
