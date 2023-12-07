cb7dc6f63d46912f90dbdc3009d7269a6b3083b6
Rizuky <mohamadrizkyramadhani17@gmail.com>
2023-12-07 14:41:00 +0700

    Menu, Lihat, Hapus

--- Code.cpp
+++ Code.cpp
@@ -16,6 +16,49 @@ void tampilkanData(const vector<Data>& data) {
     }
 }
 
+// Fungsi untuk menghapus data
+void hapusData(vector<Data>& data) {
+    string nama,pilihan;
+    
+    cout << "Apakah ingin melihat data dulu ?" << endl;
+    cout << "(Y/N) ";
+    cin >> pilihan;
+    
+    if(pilihan=="y"){
+    	
+    	tampilkanData(data);
+    	
+    	cout << "Masukan Nama yang ingin dihapus : ";
+    	cin >> nama;
+    	
+    	for(auto it = data.begin(); it !=data.end(); ++it){
+    		if(it->nama == nama){
+    			data.erase(it);
+    			cout << "Data berhasil dihapus." << endl;
+    			return;
+			}
+		}
+		
+		cout << "Data tidak ditemukan." << endl;
+	}else if(pilihan=="n"){
+		cout << "Masukan Nama yang ingin dihapus : ";
+		cin >> nama;
+		
+		for(auto it = data.begin(); it !=data.end(); ++it){
+			if(it->nama == nama){
+				data.erase(it);
+				cout << "Data berhasil dihapus." << endl;
+				return;
+			}
+		}
+		
+		cout << "Data tidak ditemukan." << endl;
+	}else{
+		cout << "Pilihan invalid" << endl;
+		return;
+	}
+}
+
 int main() {
     vector<Data> data;
 
@@ -23,8 +66,9 @@ int main() {
     while (true) {
         cout << "Menu:" << endl;
         cout << "1. Tampilkan Data" << endl;
-        cout << "2. Keluar" << endl;
-        cout << "Pilih menu (1-2): ";
+        cout << "2. Hapus Data" << endl;
+        cout << "3. Keluar"
+        cout << "Pilih menu (1-3): ";
         cin >> pilihan;
 
         switch (pilihan) {
@@ -32,7 +76,11 @@ int main() {
                 tampilkanData(data);
                 break;
             case 2:
+            	hapusData(data);
+            	break;
+            case 3:
                 return 0;
+                break;
             default:
                 cout << "Pilihan tidak valid." << endl;
         }
